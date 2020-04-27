5af331a5b376dd01954bb7c59c90b40442219798
eugenetank <e.mitnik@gmail.com>
2020-04-27 18:00:36 +0300

    Use guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { AuthComponent } from './auth/auth.component';
 import { RegisterComponent } from './register/register.component';
 
+import { AuthGuard } from './auth/auth.guard';
+
 const routes: Routes = [
   { path: 'product-list', component: ProductListComponent },
-  { path: 'auth', component: AuthComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'auth', component: AuthComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/product-list', pathMatch: 'full' },
   { path: '**', redirectTo: '/product-list' },
 ];
